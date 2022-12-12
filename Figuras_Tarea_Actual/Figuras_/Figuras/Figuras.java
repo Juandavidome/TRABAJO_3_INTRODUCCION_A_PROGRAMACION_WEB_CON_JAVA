04c3d7d10394c9f5cd5d1a6081abc5b11bb7bd99
Juan David Ome <104031677+Juandavidome@users.noreply.github.com>
2022-12-11 21:02:24 -0500

    Delete Figuras.java

--- Figuras_Tarea_Actual/Figuras_/Figuras/Figuras.java
+++
@@ -1,8 +0,0 @@
-package Figuras;
-public abstract class Figuras 
-{
-    public abstract void calcularPerimeto();
-    public abstract void calcularArea();
-    public abstract void mostrarDatos();
-}
-
